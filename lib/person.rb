e011acb41d99c08b6b8ff328e823420cd32f07a7
devin <devinlclarke@gmail.com>
2020-11-08 08:26:53 -0500

    commit tree b8abf0e768170ac3a122e4a01748a505b2593255

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,31 @@
 # your code goes here
+class Person
+
+    attr_accessor :bank_account, :happiness
+
+    def initialize(name, bank_account = 25, happiness = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+    end
+
+    def name
+        @name
+    end
+
+    def happiness
+        if @happiness > 10
+            puts 10
+        else 
+            p 8
+        end
+    end
+
+    def happy?
+        happiness > 7
+    end
+
+
+
+
+end
